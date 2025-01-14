04ba2d33375f88a71df62c24e8d633e5a5ce746b
Aniket Raj <aniketraj0224@gmail.com>
2025-01-14 19:55:38 +0530

    Add files via upload

---
+++ Lab 1 - (14.01.2023)/Test.java
@@ -0,0 +1,9 @@
+class Test
+{
+	int number;
+	public static void main(String args[])
+	{
+	Test t=new Test();
+	System.out.println("Default Value is "+t.number);
+	}
+}
---
+++ Lab 1 - (14.01.2023)/datatype.java
@@ -0,0 +1,8 @@
+public class datatype
+{
+	public static void main(String args[])
+	{
+		int a=3, b=6;
+		System.out.println("value of number is "+a+" and the other one is "+b+".");
+	}
+}
