a2b16c902298a804bc16e323af058888a8649943
Aniket Raj <aniketraj0224@gmail.com>
2025-01-21 15:27:31 +0530

    Add files via upload

---
+++ Lab 1 - (14.01.2025)/Me.java
@@ -0,0 +1,9 @@
+class Me
+{
+    public static void main (String args[])
+    {
+        System.out.println("Name: Aniket Raj");
+	System.out.println("Age: 19");
+	System.out.println("From: Barcelona");
+    }
+}
