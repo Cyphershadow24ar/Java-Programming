98765a8d5bfd48a60087d0eb231686924f002acc
Aniket Raj <aniketraj0224@gmail.com>
2025-01-21 15:26:34 +0530

    Delete Lab 1 - (14.01.2023) directory

--- Lab 1 - (14.01.2023)/Me.java
+++
@@ -1,9 +0,0 @@
-class Me
-{
-    public static void main (String args[])
-    {
-        System.out.println("Name: Aniket Raj");
-	System.out.println("Age: 19");
-	System.out.println("From: Barcelona");
-    }
-}
--- Lab 1 - (14.01.2023)/Test.java
+++
@@ -1,9 +0,0 @@
-class Test
-{
-	int number;
-	public static void main(String args[])
-	{
-	Test t=new Test();
-	System.out.println("Default Value is "+t.number);
-	}
-}
