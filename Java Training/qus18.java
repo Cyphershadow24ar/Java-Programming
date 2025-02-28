c33ef571ededf2780322d745766186c2a059d4c4
Aniket Raj <aniketraj0224@gmail.com>
2025-03-01 01:10:23 +0530

    Add files via upload

---
+++ Java Training/qus18.java
@@ -0,0 +1,16 @@
+public class qus18{
+    public static void main(String[] args) {
+        int[] arr={12,35,13,56,42};
+        int sum=0;
+        int count=0;
+        for (int x : arr){
+            System.out.print("\t" + x);
+            sum+=x;
+            count++;
+        }
+        System.out.println();
+        System.out.println(sum);
+        System.out.println(count);
+    }
+    
+}
